9b3152fc3411f7be6993bfc5efe24c3f6bd5c3aa
ssurti@oakland.edu <ssurti@oakland.edu>
2016-09-25 09:32:28 -0400

    Added the code to Ninety_Nine_Bottles_of_Beer to execute the song

--- src/beerSong/java/Main.java
+++ src/beerSong/java/Main.java
@@ -6,6 +6,19 @@ public class Main {
 	// write your code here
     }
     public static void Ninnety_Nine_Bottles_of_Beer(){
+        int i = 99;
+
+        while (i > 0){
+            System.out.print(i + " bottles on the wall ");
+            System.out.println(i + " bottles of beer");
+            System.out.print("take one down, pass it around,");
+            i = i - 1;
+            if (i > 0) {
+                System.out.println(i + " bottles of beer on the wall");
+            }
+        }
+        if (i == 0)
+            System.out.print("no more bottles on the wall ");
 
     }
 }
